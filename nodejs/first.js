628cca64647beab95e54f2bc1f3f23a979009438
liker <liker365@163.com>
2014-04-20 15:52:52 +0800

    first.js

--- nodejs/first.js
+++ nodejs/first.js
@@ -1,1 +1,10 @@
-first nodejs project!
+var http = require('http');  
+var os = require('os');  
+  
+http.createServer(function (request, response) {  
+  response.writeHead(200, {'Content-Type': 'text/plain'});  
+  response.end('Hello World\n');  
+  
+}).listen(8124);  
+ 
+console.log('Server running at http://127.0.0.1:8124/');
