0ca979e4c66d940ffcf763abd02201e4073575ae
liker <liker365@163.com>
2014-04-22 00:41:57 +0800

    alter 201404220040

--- java/Test.java
+++ java/Test.java
@@ -1,5 +1,6 @@
 public class Test {
 	public static void main(String[] args) {
-		System.out.print("Hello , this is my first test on sublime !");
+		System.out.println("Hello , this is my first test on sublime !");
+		System.out.println(MyUtils.getNowTime());
 	}
 }
