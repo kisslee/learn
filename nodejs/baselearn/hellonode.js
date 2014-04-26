86e3ae3fcb7803e5ad0b21c10ab754d2dcd45635
liker <liker365@163.com>
2014-04-26 19:04:52 +0800

    mod hellonode.js

--- nodejs/baselearn/hellonode.js
+++ nodejs/baselearn/hellonode.js
@@ -1,6 +1,12 @@
 var http = require('http');
-http.createServer(function(req,res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello Node\n');
-}).listen(9001, "127.0.0.1");
+http.createServer(function(request,response){
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello Node\n');
+}).listen(9001);
+
 console.log('Server running at http://127.0.0.1:9001/');
+console.time('100-elements');
+for (var i = 0; i < 100; i++) {
+    ;
+}
+console.timeEnd('100-elements');
